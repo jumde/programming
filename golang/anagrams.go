9ccfa4c1fa11fed2725b8bd67e4fc4ce4c19910a
Pranjal Jumde <pranjal@brave.com>
2020-10-15 21:35:41 -0700

    anagrams

---
+++ golang/anagrams.go
@@ -0,0 +1,29 @@
+/*
+  Check for anagrams
+*/
+
+package main
+
+import "fmt"
+
+func isAnagram(str1 string, str2 string)  bool {
+    var charSet = make(map[string]int)
+    for _, c := range str1 {
+        charSet[string(c)] += 1
+    }
+    for _, c := range str2 {
+        charSet[string(c)] -= 1
+    }
+    for _, val := range charSet {
+        if val != 0 {
+            return false
+        }
+    }
+    return true
+}
+
+func main() {
+    fmt.Printf("fired/fried: %v\n", isAnagram("fired", "fried"));
+    fmt.Printf("listen/silent: %v\n", isAnagram("listen", "silent"));
+    fmt.Printf("yolo/bolo: %v\n", isAnagram("yolo", "bolo"));
+}
